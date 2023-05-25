4c59140496ecfd4476298ae48631af2abcfd1f09
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-05-25 09:28:22 -0300

    Exercícios 18 a 20

---
+++ UNIP/Ex018/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ex018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que leia um número inteiro (variável CODIGO). Verificar se o código é igual a 1, igual a 2 ou
+              igual a 3. Caso não seja, apresentar a mensagem “Código inválido”. Ao ser verificado o código e constatado que é
+              um valor válido, o programa deve verificar cada código em separado para determinar seu valor por extenso, ou
+              seja, apresentar a mensagem “um”, ”dois” ou “três”. (Utilizar o comando Switch).*/
+
+            try
+            {
+                Console.Write("Digite um número inteiro: "); int codigo = int.Parse(Console.ReadLine());
+                Console.WriteLine($"\nResultado: {GeraNumeroPorExtenso(codigo)}");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        private static string GeraNumeroPorExtenso(int codigo)
+        {
+            switch (codigo)
+            {
+                case 1:
+                    return "Um";
+                case 2:
+                    return "Dois";
+                case 3:
+                    return "Três";
+                default:
+                    return "Inválido";
+            }
+        }
+    }
+}
---
+++ UNIP/Ex019/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ex019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Escreva um programa que leia três valores para os lados de um triângulo (variáveis A, B e C). Verificar se cada
+               lado é menor que a soma dos outros dois lados. Se sim, saber de A==B e se B==C, sendo verdade o triângulo é
+               eqüilátero; Se não, verificar de A==B ou se A==C ou se B==C, sendo verdade o triângulo é isósceles; e caso
+               contrário, o triângulo será escaleno. Caso os lados fornecidos não caracterizarem um triângulo, avisar a
+               ocorrência.*/
+
+            try
+            {
+                Console.Write("Digite o valor de A: "); double a = double.Parse(Console.ReadLine());
+                Console.Write("Digite o valor de B: "); double b = double.Parse(Console.ReadLine());
+                Console.Write("Digite o valor de C: "); double c = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"\nTriângulo {VerificaTipoTriangulo(a, b, c)}");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        private static object VerificaTipoTriangulo(double a, double b, double c)
+        {
+            if (TrianguloPossivel(a, b, c))
+            {
+                if (a == b && b == c) return "Equilatero";
+                else if (a == b || a == c || b == c) return "Isóceles";
+                else return "Escaleno";
+            }
+            else 
+                return "Inválido";
+        }
+
+        private static bool TrianguloPossivel(double a, double b, double c)
+        {
+            return a + b > c && a + c > b && b + c > c;
+        }
+    }
+}
