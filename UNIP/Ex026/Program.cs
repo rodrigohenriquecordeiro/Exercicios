ee791380a9ffe6987214674ece539705e6e0c81f
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-05-27 15:04:51 -0300

    Exercícios 26 e 27

---
+++ UNIP/Ex026/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente.*/
+
+            try
+            {
+                for (int i = 1; i <= 100; i++) Console.WriteLine($"{101 - i}");
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ UNIP/Ex027/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ex027
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que leia:
+              - a quantidade de números que deverá processar;
+              - os números que deverá processar,e calcule e exiba, para cada número a ser processado o seu fatorial.
+              Lembrete: O fatorial de um número N é dado pela fórmula: N! = 1 * 2 * 3 * 4 * 5 * ... * N*/
+
+            try
+            {
+                Console.Write("Qual a quantidade de números para processar? "); int num = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Resultado: {Fatorial(num)}");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        private static string Fatorial(int num)
+        {
+            int fatorial = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                fatorial *= i;
+            }
+
+            return fatorial.ToString();
+        }
+    }
+}
