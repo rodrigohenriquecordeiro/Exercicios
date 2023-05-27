ff851edeb1e98ffe37f458a2f8038ce280ee5a14
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-05-27 14:51:25 -0300

    Exercícios 24 e 25

---
+++ UNIP/Ex024/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Ex024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que leia dois números inteiros e apresente as opções para usuário escolher o que deseja  realizar:
+              1 – Verificar se um dos números lidos é ou não múltiplo do outro
+              2 – Verificar se os dois números lidos são pares
+              3 – Verificar se a média dos dois números é maior ou igual a 7.
+              4 – Sair*/
+
+            try
+            {
+                Console.Write("Digite o primeiro número: "); int n1 = int.Parse(Console.ReadLine());
+                Console.Write("Digite o segundo número: "); int n2 = int.Parse(Console.ReadLine());
+
+                Console.Write("\n1 – Verificar se um dos números lidos é ou não múltiplo do outro" +
+                    "\n2 – Verificar se os dois números lidos são pares" +
+                    "\n3 – Verificar se a média dos dois números é maior ou igual a 7." +
+                    "\n4 – Sair");
+
+                Console.Write("\nDigite sua escolha: ");
+                int escolha = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Resultado: {ExecutaOperacao(n1, n2, escolha)}"); 
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        private static string ExecutaOperacao(int n1, int n2, int escolha)
+        {
+            return escolha switch
+            {
+                1 => Multiplo(n1, n2),
+                2 => Par(n1, n2),
+                3 => Media(n1, n2).ToString("F2"),
+                _ => ""
+            };
+        }
+
+        private static double Media(int n1, int n2)
+        {
+            return (n1 + n2) / 2;
+        }
+
+        private static string Par(int n1, int n2)
+        {
+            if (n1 % 2 == 0 && n2 % 2 == 0)
+                return $"{n1} e {n2} são par";
+            else if (n1 % 2 == 0 && n2 % 2 != 0)
+                return $"{n1} é par e {n2} é ímpar";
+            else if (n1 % 2 != 0 && n2 % 2 == 0)
+                return $"{n1} é ímpar e {n2} é par";
+            else
+                return $"{n1} e {n2} são impares";
+        }
+
+        private static string Multiplo(int n1, int n2)
+        {
+            if (n1 % n2 == 0 || n2 % n1 == 0)
+                return "É multiplo";
+            else
+                return "Não é multiplo";
+        }
+    }
+}
---
+++ UNIP/Ex025/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Tendo como dados de entrada a altura e o sexo de uma pessoa, faça um programa que calcule seu peso ideal,
+             * utilizando as seguintes fórmulas: (h = altura)
+            - Para homens: (72.7*h) - 58
+            - Para mulheres: (62.1 *h) - 44.7*/
+
+            try
+            {
+                Console.Write("Qual a sua altura? "); double altura = double.Parse(Console.ReadLine());
+                Console.Write("Qual o seu sexo? "); char sexo = char.Parse(Console.ReadLine());
+
+                if (sexo == 'M')
+                    Console.WriteLine($"\nPeso ideal: {72.7 * altura - 58:F2}.");
+
+                if (sexo == 'F')
+                    Console.WriteLine($"\nPeso ideal: {62.1 * altura - 44.7:F2}.");
+
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
