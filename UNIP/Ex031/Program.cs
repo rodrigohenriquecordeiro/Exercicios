40b7b6592cad96dc845c83a3c209019b0fa79586
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-05-29 07:28:53 -0300

    Exercícios 31 e 32

---
+++ UNIP/Ex031/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex031
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escrever um programa que leia um conjunto de números positivos, e exiba se o número lido é par ou ímpar. Exiba
+              ao final a soma total dos números pares lidos e também a soma dos números ímpares lidos. Suporemos que o
+              número de elementos deste conjunto não é conhecido, e que um número negativo será utilizado para sinalizar o
+              fim dos dados.*/
+
+            try
+            {
+                int numeros = 0;
+                List<int> listaDeNumeros = new List<int>();
+
+                Console.WriteLine("Digite vários números, quando terminar tecle -1.");
+                
+                while (numeros > -1) 
+                {
+                    Console.Write("Número: "); numeros = int.Parse(Console.ReadLine());
+                    listaDeNumeros.Add(numeros);
+                }
+
+                string numerosEscolhidos = string.Empty;
+                foreach (var item in listaDeNumeros.OrderBy(x => x).ToList())
+                {
+                    if (item > -1) numerosEscolhidos += item + ", ";
+                }
+                Console.WriteLine($"\nNúmeros escolhidos: {numerosEscolhidos.Substring(0, numerosEscolhidos.Trim().Length - 1)}");
+
+                Console.WriteLine(GeradorDeNumeros(listaDeNumeros)); 
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        private static string GeradorDeNumeros(List<int> numeros)
+        {
+            int par = 0, impar = 0;
+            
+            foreach (var item in numeros.Where(x => x > 0).ToList())
+            {
+                if (item % 2 == 0)
+                    par += item;
+                else
+                    impar += item;
+            }
+
+            return $"\nSoma dos números pares = {par}." +
+                $"\nSoma dos números ímpares = {impar}.";
+        }
+    }
+}
---
+++ UNIP/Ex032/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Ex032
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que calcule e exiba a soma dos quadrados dos 20 primeiros números inteiros positivos
+              ímpares a partir do número informado pelo usuário menor que 10 e maior que zero.*/
+
+            try
+            {
+                int numeroDigitado = 0;
+                Console.Write("Digite um número maior do que 0 e menor do que 10: ");
+
+                while (ValidaNumero(out numeroDigitado))
+                {
+                    Console.Write("\nInválido, tente novamente: ");
+                }
+                Console.WriteLine($"\nNúmero escolhido: {numeroDigitado}");
+                Console.WriteLine($"Resultado: {GeraResultado(numeroDigitado)}");
+
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        private static bool ValidaNumero(out int numeroDigitado)
+        {
+            return !int.TryParse(Console.ReadLine(), out numeroDigitado) && numeroDigitado > 0 && numeroDigitado <= 10;
+        }
+
+        private static string GeraResultado(int numeroDigitado)
+        {
+            int soma = 0;
+            for (int i = numeroDigitado; i <= 20; i++)
+            {
+                if (numeroImpar(numeroDigitado))
+                {
+                    soma += numeroDigitado * numeroDigitado;
+                }
+            }
+
+            return soma.ToString(); 
+        }
+
+        private static bool numeroImpar(int numeroDigitado)
+        {
+            return numeroDigitado % 2 != 0;
+        }
+    }
+}
