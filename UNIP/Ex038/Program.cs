aa5a39c0f4b60105147ed4a16c70d615e8e72d73
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-06-04 13:38:36 -0300

    Exercícios 38 e 39

---
+++ UNIP/Ex038/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ex038
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa que leia as variáveis C e N, respectivamente código e número de horas trabalhadas de um
+              operário. E calcule o salário sabendo-se que ele ganha R$ 10,00 por hora. Quando o número de horas exceder a
+              50, calcule o excesso de pagamento armazenando-o na variável E, caso contrário zerar tal variável. A hora
+              excedente de trabalho vale R$ 20,00. No final do processamento imprimir o salário total e o salário excedente. O
+              programa só deve parar de rodar quando o usuário responder "S" na seguinte pergunta, "Deseja encerrar o
+              programa?".*/
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Cálculo de Horas Trabalhada");
+                    Console.Write("Qual o código? "); int codigo = int.Parse(Console.ReadLine());
+                    Console.Write("Quantas horas o funcionário trabalhou? "); decimal horasTrabalhadas = decimal.Parse(Console.ReadLine());
+                    Console.WriteLine(CalculaSalario(codigo, horasTrabalhadas)); 
+
+                    Console.Write("\nDeseja encerrar o programa? "); string encerrar = Console.ReadLine();
+                    if (encerrar.ToUpper() == "S")
+                        break;
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        private static string CalculaSalario(int codigo, decimal horasTrabalhada)
+        {
+            const decimal horaBase = 10;
+            const decimal horaExcedente = 20;
+            const int limitedeHoras = 50;
+            decimal excessoDePagamento = 0;
+
+            if (horasTrabalhada <= limitedeHoras)
+            {
+                return $"\nCódigo do Funcionário: {codigo}." +
+                    $"\nSalário Excedente: {excessoDePagamento:C}." +
+                    $"\nSalário Total: {horaBase * horasTrabalhada:C}.";
+            }
+            else
+            {
+                excessoDePagamento = (horasTrabalhada - limitedeHoras) * horaExcedente;
+                return $"\nCódigo do Funcionário: {codigo}." +
+                    $"\nSalário Excedente: {excessoDePagamento:C}." +
+                    $"\nSalário Total: {horaBase * limitedeHoras + excessoDePagamento:C}.";
+            }
+        }
+    }
+}
---
+++ UNIP/Ex039/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Ex039
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa que leia um número inteiro e mostre uma mensagem indicando se este número é par ou ímpar,
+              e se é positivo ou negativo. O programa só deve parar de rodar quando o usuário responder "S" na seguinte
+              pergunta, "Deseja encerrar o programa?" .*/
+
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Digite um número inteiro: "); int numero = int.Parse(Console.ReadLine());
+                    Console.WriteLine(PositivoOuNegativo(numero));
+                    Console.WriteLine(ParOuImpar(numero));
+                    Console.Write("\nDeseja encerrar o programa? "); string continuar = Console.ReadLine();
+                    if (continuar.ToUpper() == "S")
+                        break;
+                }
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        private static string PositivoOuNegativo(int numero)
+        {
+            if (numero < 0)
+                return $"O número {numero} é negativo.";
+            else
+                return $"O número {numero} é positivo.";
+        }
+
+        private static string ParOuImpar(int numero)
+        {
+            if (numero % 2 == 0)
+                return $"O número {numero} é Par.";
+            else
+                return $"O número {numero} é Ímpar.";
+        }
+    }
+}
