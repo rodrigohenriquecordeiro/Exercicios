ea29374821ac259940be864ac37236e95c12d1b6
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-06-18 16:44:11 -0300

    Exercícios 55 e 56

---
+++ UNIP/Ex055/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex055
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa que leia uma frase de até 50 caracteres e imprima a frase sem os espaços em branco.
+              Imprimir também a quantidade de espaços em branco da frase.*/
+
+            try
+            {
+                string frase = "Antes de P e B, só M devo escrever. Antes das outras letras, só o N meio perneta!";
+                int contador = 0;
+
+                for (int i = 0; i < frase.Length; i++)
+                {
+                    if (frase[i] == ' ') contador++;
+                }
+
+                Console.WriteLine($"{frase.Replace(" ", "")}" +
+                    $"\nQuantidade de espaços: {contador}");
+
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ UNIP/Ex056/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex056
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa para ler um vetor de inteiros de 50 posições. Imprimir a quantidade de números pares e de múltiplos de 5.*/
+
+            try
+            {
+                int[] vetor = new int[20] { 1, 2, 3, 36, 45, 36, 21, 586, 14, 236, 125, 154, 52, 2165, 20, 36, 40, 50, 56, 36 };
+                string resultado = string.Empty;
+                
+                foreach (var numero in vetor)
+                {
+                    if (numero % 2 == 0 && numero % 5 == 0)
+                        resultado += numero.ToString() + ", ";
+                }
+                Console.WriteLine($"Resultado: {resultado.Substring(0, resultado.Trim().Count() - 1)}");
+
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
