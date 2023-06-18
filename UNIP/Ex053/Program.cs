601aca8597261b7a8a660e9312313c4f02573447
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-06-18 16:19:28 -0300

    Exercícios 53 e 54

---
+++ UNIP/Ex053/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex053
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Durante uma corrida de automóveis com N voltas de duração foram anotados para um piloto, na ordem, os
+              tempos registrados em cada volta. Fazer um programa para ler os tempos das N voltas, calcular e imprimir:
+              i. melhor tempo;
+              ii. a volta em que o melhor tempo ocorreu;
+              iii. tempo médio das N voltas;*/
+
+            try
+            {
+                double tempo = 0;
+                List<double> list = new List<double>();
+
+                while (tempo != -1)
+                {
+                    Console.Write("Digite o tempo da volta: ");
+                    list.Add(tempo = double.Parse(Console.ReadLine()));
+                }
+
+                Console.WriteLine($"\nMelhor tempo: {list.Where(x => x > 0).Min()}" +
+                    $"\nVolta em que o tempo ocorreu: {list.IndexOf(list.Where(x => x > 0).Min()) + 1}" +
+                    $"\nTempo médio: {list.Where(x => x > 0).Average():F2}");
+
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ UNIP/Ex054/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex054
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dado dois vetores, A (5 elementos) e B (8 elementos), faça um programa que imprima todos os elementos comuns aos dois vetores*/
+
+            try
+            {
+                int[] a = new int[5] { 1, 2, 3, 4, 5};
+                int[] b = new int[8] { 2, 3, 5, 6, 7, 8, 9, 10};
+                List<int> lista = new List<int>();
+
+                foreach (var numero in a) foreach (var item in b) if (numero == item) lista.Add(item);
+                
+                foreach (var item in lista) Console.Write($"{item}, ");
+
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
