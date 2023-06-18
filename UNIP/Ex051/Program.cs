49a449f96cd80cae838f86c06d2c9fda11bb34df
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-06-18 14:47:18 -0300

    Exercícios 51 e 52

---
+++ UNIP/Ex051/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex051
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                /*Fazer um programa para ler uma quantidade N de alunos. Ler a nota de cada um dos N alunos e calcular a média
+                  aritmética das notas. Contar quantos alunos estão com a nota acima de 7.0. Obs.: Se nenhum aluno tirou nota
+                  acima de 5.0, imprimir mensagem: Não há nenhum aluno com nota acima de 5.*/
+
+                List<double> lstNotas = new List<double>();
+
+                Console.Write("Quantas notas serão cadastradas: "); int quantidadeDeNotas = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= quantidadeDeNotas; i++)
+                {
+                    Console.Write($"{i}º nota: "); double nota = double.Parse(Console.ReadLine());
+                    lstNotas.Add(nota);
+                }
+
+                Console.WriteLine($"\nQuantidade de Notas: {quantidadeDeNotas}" +
+                    $"\nMédia: {lstNotas.Sum() / quantidadeDeNotas}" +
+                    $"\nTotal de Alunos com nota acima de 7: {lstNotas.Where(x => x >= 7).Count()}" +
+                    $"\nTotal de Alunos com nota abaixo de 7: {lstNotas.Where(x => x < 5).Count()}");
+
+                Console.WriteLine("\nFim");
+                Console.ReadLine();
+            }
+			catch (Exception e)
+			{
+				throw new Exception(e.Message);
+			}
+        }
+    }
+}
---
+++ UNIP/Ex052/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex052
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa ler um vetor de inteiros e positivos e imprimir quantas vezes aparece o número 1, 3 e 4, nesta
+              ordem. O vetor terá no máximo 100 posições. Sair do programa quando for digitado -1.*/
+
+            try
+            {
+                int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                int[] sequencia = { 3, 4, 5 };
+
+                int sequenciaLength = sequencia.Length;
+                int count = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == sequencia[count])
+                    {
+                        count++;
+
+                        if (count == sequenciaLength)
+                        {
+                            Console.WriteLine("Sequência encontrada na posição {0}", i - sequenciaLength + 1);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                }
+
+                if (count != sequenciaLength)
+                {
+                    Console.WriteLine("Sequência não encontrada");
+                }
+
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
