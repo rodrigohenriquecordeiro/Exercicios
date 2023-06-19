6f03c3520895bba9786c3e8178dfcaf517a313fe
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-06-19 19:49:23 -0300

    Exercícios 57 e 58

---
+++ UNIP/Ex057/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex057
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa que leia uma frase e imprima somente as vogais.*/
+
+            try
+            {
+                Console.Write("Escreva uma frase: "); 
+                string frase = Console.ReadLine();
+                string letra = string.Empty;
+
+                for (int i = 0; i < frase.Length; i++)
+                {
+                    letra = frase.ToUpper().Substring(i, 1);
+                    if (letra == "A" || letra == "E" || letra == "I" || letra == "O" || letra == "U")
+                        Console.Write(letra);
+                    else
+                        Console.Write("_");
+                }
+                
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+    }
+}
---
+++ UNIP/Ex058/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex058
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa para armazenar em um vetor, vários números inteiros e positivos e calcular a média. Imprimir
+              também o maior. A quantidade de números lidos será definida pelo usuário.*/
+
+            try
+            {
+                Console.Write("Quantos números você quer utilizar? "); int quantidade = int.Parse(Console.ReadLine());
+                int[] vetor = new int[quantidade];
+
+                for (int i = 1; i <= quantidade; i++)
+                {
+                    Console.Write($"Digite o {i}º número: ");
+                    vetor[i - 1] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine($"\nMédia: {vetor.Average():F2}" +
+                    $"\nMaior: {vetor.Max()}");
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
