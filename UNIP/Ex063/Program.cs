c9b3c36404dc4bc21d24d1b62ece8424210dd626
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-06-20 20:30:27 -0300

    Iniciado exercício 64

---
+++ UNIP/Ex063/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex063
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que verifique se um número é par ou ímpar através de um método chamado VERIFICA. O
+              método deverá receber um número inteiro (n) e deverá retornar a mensagem “PAR” ou “ÍMPAR”.*/
+
+            try
+            {
+                Console.Write("Digite um número: "); int numero = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Resultado: {ParOuImpar(numero)}");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            static string ParOuImpar(int numero)
+            {
+                if (numero % 2 == 0)
+                    return "PAR";
+                else
+                    return "ÍMPAR";
+            }
+        }
+    }
+}
---
+++ UNIP/Ex064/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Ex064
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que deverá ter as seguintes opções:
+              1 - Carregar Vetor
+              2 - Listar Vetor
+              3 - Exibir apenas os números pares do vetor
+              4 - Exibir apenas os números ímpares do vetor
+              5 - Exibir a quantidade de números pares existem nas posições ímpares do vetor
+              6 - Exibir a quantidade de números ímpares existem nas posições pares do vetor
+              7 - Sair
+              Deverá ser implementado um método para realizar cada uma das opções de 1 a 6*/
+
+            try
+            {
+                int[] vetor = null;
+
+                while (true)
+                {
+                    Console.Write("OPÇÕES:" +
+                        "\n1 - Carregar Vetor" +
+                        "\n2 - Listar Vetor" +
+                        "\n3 - Exibir apenas os números pares do vetor" +
+                        "\n4 - Exibir apenas os números ímpares do vetor" +
+                        "\n5 - Exibir a quantidade de números pares existem nas posições ímpares do vetor" +
+                        "\n6 - Exibir a quantidade de números ímpares existem nas posições pares do vetor" +
+                        "\n7 - Sair" +
+                        "\nDigite sua escolha: ");
+
+                    int escolha = 0;
+                    while (!int.TryParse(Console.ReadLine(), out escolha) || escolha < 1 || escolha > 7)
+                    {
+                        Console.WriteLine("\nOpção inválida!");
+                        Console.Write("Digite novamente sua escolha: ");
+                    }
+
+                    if (escolha == 1)
+                    {
+                        CarregarVetor(vetor);
+                    }
+
+                    
+
+                    if (escolha == 2)
+                        ListarVetor(vetor);
+
+                    //1 => CarregarVetor(),
+                    //2 => ListarVetor(),
+                    //3 => Par(),
+                    //4 => Impar(),
+                    //5 => QuantidadeImpares(),
+                    //6 => QuantidadePares(),    
+                }
+
+
+                Console.WriteLine("Fim");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }                    
+
+            static int[] CarregarVetor(int[] vetor)
+            {
+                Console.Write("Qual o tamanho do vetor? ");
+                int tamanho = int.Parse(Console.ReadLine());
+                vetor = new int[tamanho];
+
+                for (int i = 0; i < tamanho; i++)
+                {
+                    Console.Write($"{i + 1}º número: ");
+                    vetor[i] = int.Parse(Console.ReadLine());
+                }
+
+                return vetor;
+            }
+
+            static void ListarVetor(int[] vetor)
+            {
+                foreach (var numero in vetor)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+        }
+    }
+}
